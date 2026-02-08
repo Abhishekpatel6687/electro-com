708ee9596c12050e36895be853035d3d745a605a
Abhishek Patel <Abhishekpatel6687@gmail.com>
2026-02-08 14:50:47 +0530

    some message

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,32 @@
+// import pkg from "pg";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const { Pool } = pkg;
+
+// export const pool = new Pool({
+//   // user: process.env.DB_USER,
+//   // password: process.env.DB_PASSWORD,
+//   // database: process.env.DB_NAME,
+//   // host: process.env.DB_HOST,
+//   // port: process.env.DB_PORT,
+//   connectionString: process.env.DATABASE_URL,
+//   ssl:
+//     process.env.NODE_ENV === "production"
+//       ? { rejectUnauthorized: false }
+//       : false,
+//   // });
+//   //   ssl: process.env.DB_HOST !== "localhost"
+//   //     ? { rejectUnauthorized: false }
+//   //     : false
+// });
+
+// pool
+//   .connect()
+//   .then(() => console.log("PostgreSQL connected ✅"))
+//   .catch((err) => console.error("PostgreSQL connection error ❌", err));
+
 import pkg from "pg";
 import dotenv from "dotenv";
 
@@ -6,23 +35,15 @@ dotenv.config();
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME,
-  // host: process.env.DB_HOST,
-  // port: process.env.DB_PORT,
   connectionString: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
-  // });
-  //   ssl: process.env.DB_HOST !== "localhost"
-  //     ? { rejectUnauthorized: false }
-  //     : false
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool
   .connect()
   .then(() => console.log("PostgreSQL connected ✅"))
-  .catch((err) => console.error("PostgreSQL connection error ❌", err));
+  .catch((err) =>
+    console.error("PostgreSQL connection error ❌", err)
+  );
