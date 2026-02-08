667c5063cab7e1fabe75b5415a976bb754b57e61
Abhishek Patel <Abhishekpatel6687@gmail.com>
2026-02-08 17:08:00 +0530

    some message

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,34 +1,10 @@
-import express from "express";
 import pg from "pg";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 10000;
-
-const pool = new pg.Pool({
+export const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false, // Render pe PostgreSQL ke liye required
   },
-});
-
-pool.connect()
-  .then(() => console.log("PostgreSQL connected ✅"))
-  .catch(err => console.error("PostgreSQL connection error ❌", err));
-
-// Sample route to test table
-app.get("/products", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM productfilter");
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Database error");
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
