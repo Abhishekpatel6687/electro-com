b745455b7d28af54e2cb9d7b4ac1236ae33488f7
Abhishek Patel <Abhishekpatel6687@gmail.com>
2026-02-11 00:04:36 +0530

    cookie install and save

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import { pool } from "./config/db.js";
 import express from "express";
+import cookieParser from "cookie-parser";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,9 +13,17 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
-app.use(express.json());
+// frontend me cookie use krna hai to backend me cors me credentials true krna hoga aur origin me frontend ka url dena hoga.
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000", // frontend ka URL
+//     credentials: true,               // ⭐ MUST
+//   })
+// );
 
+app.use(cors()); // CORS ko globally enable kar diya, specific origin ke bina
+app.use(express.json());
+app.use(cookieParser());
 // Serve uploads folder correctly
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
@@ -37,7 +47,8 @@ app.get("/check-db", async (req, res) => {
     res.json(r.rows);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ error: err.message, });
+
   }
 });
 
@@ -47,6 +58,17 @@ app.get("/check-db", async (req, res) => {
 app.get("/", (req, res) => {
   res.cookie("name", "abhishek");
   res.send("done")
+});
+
+app.get("/get-cookie", (req, res) => {
+  const name = req.cookies.name;
+  console.log(req.cookies);
+  res.send(name);
+});
+
+app.get("/ab", (req, res) => {
+  console.log("req.cookies", req.cookies); // ⭐ debug
+  res.json(req.cookies);
 });
 
 
